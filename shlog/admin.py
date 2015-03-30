f62848c6746efee2d1075cc099c2d1e9adae94f1
Steve McBurnett (j007bond) <j007bond@oit.rutgers.edu>
2015-03-30 10:39:40 -0400

    Added JournalAuthor and CharacterStatus to Admin inclusion

--- shlog/admin.py
+++ shlog/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import JournalEntry, JournalSection, SectionGroup
+from .models import JournalEntry, JournalSection, SectionGroup, JournalAuthor, CharacterStatus
 
 
 admin.site.register(JournalEntry)
 admin.site.register(JournalSection)
 admin.site.register(SectionGroup)
+admin.site.register(JournalAuthor)
+admin.site.register(CharacterStatus)
